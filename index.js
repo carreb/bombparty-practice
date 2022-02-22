0c511544d42fe4f58689db59b18de2c8c2a7db83
carreb <carrebishere@gmail.com>
2022-02-22 13:25:34 -0500

    render the current prompt on screen

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
   const RANDOM_PROMPT_URL = 'http://127.0.0.1:443/randomprompt'
+  const promptDisplay = document.getElementById('prompt-display')
 
 
 function getRandPrompt() {
@@ -8,10 +9,11 @@ function getRandPrompt() {
     .then(data => data.prompt)
 }
 
-async function getNextPrompt() {
+async function renderPrompt() {
     const prompt = await getRandPrompt();
     console.log("The following is the letter prompt:")
     console.log(prompt)
+    promptDisplay.innerText = prompt
 }
 
-getNextPrompt()
+renderPrompt()
