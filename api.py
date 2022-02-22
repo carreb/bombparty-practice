64412e1da6d41a377a2423fd0002fe99d51a41c8
carreb <32069843+carreb@users.noreply.github.com>
2022-02-22 07:48:33 -0500

    check for letter h implement

--- api.py
+++ api.py
@@ -21,6 +21,25 @@ def get_hyphenated_words():
           h = hyphen.replace('\u00e2\u20ac\u2122', "'")
           hyphenList.append(h.lower())
         return json.dumps({'response': hyphenList})
+@app.route('/cflhyphenated/<inputword>,<promptedletters>')
+def check_for_letters_in_word_hyphenated(inputword, promptedletters):
+    with open('./dict/hyphen-dict.txt') as f:
+        hyphenatedwords = f.readlines()
+        hyphenList = []
+        for word in hyphenatedwords:
+            hyphen = word.replace('\n', '')
+            h = hyphen.replace('\u00e2\u20ac\u2122', "'")
+            hyphenList.append(h.lower())
+        if promptedletters in inputword:
+            if inputword in hyphenList:
+                if '-' in inputword:
+                    return json.dumps({'response': 'true'})
+                else:
+                    return json.dumps({'response': 'false', 'reason': 'no hyphen'})
+            else:
+                return json.dumps({'response': 'false', 'reason': 'word not in list', 'word': inputword.lower()})
+        else:
+            return json.dumps({'response': 'false', 'reason': 'no prompt letters in word'})
 
 
 if __name__ == 'main':
