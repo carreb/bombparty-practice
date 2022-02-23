45eb3be56a3957f6a2157adac1a905d870e7d90b
carreb <32069843+carreb@users.noreply.github.com>
2022-02-23 00:05:17 -0500

    check for valid word on submit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
   const RANDOM_PROMPT_URL = 'http://127.0.0.1:443/randomprompt'
+  const HYPHENATED_CHECK_URL = 'http://127.0.0.1:443/cflhyphenated/'
   const promptDisplay = document.getElementById('prompt-display')
   const wordInput = document.getElementById('wordInput')
   const submitButton = document.getElementById('submitButton')
@@ -32,9 +33,31 @@ wordInput.addEventListener('input', () => {
 
 wordInput.addEventListener("keydown", function(e){
   if (e.key === "Enter") {
-    wordInput.value = null
+    var cachedValue = wordInput.value.toLowerCase()
     promptDisplay.classList.remove('correctletters')
-  }
+    getCorrectResponse(cachedValue)
+    wordInput.value = null
+    }
 })
+
+function checkCorrect(inputVal) {
+  return fetch(HYPHENATED_CHECK_URL + inputVal + ',' + promptRelated.prompt)
+  .then(response => response.json())
+  .then(data => data.response)
+}
+
+async function getCorrectResponse(inputVal) {
+  var correctResponse = await checkCorrect(inputVal)
+  if (correctResponse === "true") {
+    console.log("Valid word")
+  }
+  else if (correctResponse === "false") {
+    console.log("Invalid word")
+    fetch(HYPHENATED_CHECK_URL + inputVal + ',' + promptRelated.prompt)
+      .then(response => response.json())
+      .then(data => console.log("reason: " + data.reason))
+
+  }
+}
 
 renderPrompt()
